df7cc3e27189ff0bed586edfc93b9cfb213bd74a
Peter Ukena <peter.ukena@brille24.de>
2019-06-16 19:41:59 +0200

    WIP: Fixed admin routing config again, changed menu location

--- src/Menu/AdminMenuListener.php
+++ src/Menu/AdminMenuListener.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Kortwotze\FeedsPlugin\Menu;
 
+use Knp\Menu\ItemInterface;
 use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
 
 final class AdminMenuListener
@@ -17,12 +18,13 @@ final class AdminMenuListener
     public function addAdminMenuItem(MenuBuilderEvent $event): void
     {
         $menu = $event->getMenu();
-        $feedsMenu = $menu
-            ->addChild('new')
-            ->setLabel('kortwotze.feeds_plugin.menu.section_title');
+        $marketingMenu = $menu->getChild('marketing');
 
-        $feedsMenu
-            ->addChild('new-subitem', ['route' => 'kortwotze_feeds_plugin_admin_dashboard'])
-            ->setLabel('kortwotze.feeds_plugin.menu.general_configuration');
+        if ($marketingMenu instanceof ItemInterface) {
+            $marketingMenu
+                ->addChild('feeds', ['route' => 'kortwotze_feeds_plugin_admin_dashboard'])
+                ->setLabel('kortwotze.feeds_plugin.menu.general_configuration')
+                ->setLabelAttribute('icon', 'rss-square');
+        }
     }
 }
